ca1eb21737b407de1e7f8991fdd58395b2972102
Mahendra Kalkura <mahendra@kalkura.com>
2017-03-30 02:20:22 +0530

    resources/js/all.js

--- resources/js/all.js
+++ resources/js/all.js
@@ -3,25 +3,13 @@ var url = document.querySelector('body').getAttribute('data-url');
 var root = document.querySelector('#root');
 
 var oninit = function() {
-    var socket = new WebSocket('ws://' + url + '/websockets/');
-
-    socket.addEventListener('open', function(event) {
-        socket.send('start');
-    });
-
-    socket.addEventListener('close', function(event) {
-    });
-
-    socket.addEventListener('error', function(event) {
-    });
-
-    socket.addEventListener('message', function(event) {
-        component.progress = event.data;
-        m.redraw();
-        if (event.data === 100) {
-            socket.close();
-        }
-    });
+    component.progress = component.progress + 1;
+    m.redraw();
+    if (component.progress < 100) {
+        setTimeout(function() {
+            oninit();
+        }, 100);
+    }
 };
 
 var component = {
