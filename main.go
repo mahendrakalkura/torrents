dc14965dfaa0b91399e17a37155f38c586623da6
Mahendra Kalkura <mahendra@kalkura.com>
2017-03-29 16:20:30 +0530

    main.go

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"flag"
+
+	"github.com/mahendrakalkura/torrents/go/actions"
+)
+
+func main() {
+	action := flag.String("action", "", "")
+	flag.Parse()
+	if *action == "serve" {
+		actions.Serve()
+	}
+}
