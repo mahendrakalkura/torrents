687e6b47528aad7e5ef4b5ee6969bbae6c877dc8
Mahendra Kalkura <mahendra@kalkura.com>
2017-03-30 11:20:32 +0530

    resources/js/all.js

--- resources/js/all.js
+++ resources/js/all.js
@@ -3,32 +3,67 @@ var url = document.querySelector('body').getAttribute('data-url');
 var root = document.querySelector('#root');
 
 var oninit = function() {
-    component.progress = component.progress + 1;
-    m.redraw();
-    if (component.progress < 100) {
-        setTimeout(function() {
-            oninit();
-        }, 100);
-    }
+    component.items = [];
+    component.isLoading = true;
+    component.isSuccess = false;
+    component.isFailure = false;
+    m.request({method: 'GET', url: url + '/items/'})
+    .then(
+        function(items) {
+            component.items = items;
+            component.isLoading = false;
+            component.isSuccess = true;
+            component.isFailure = false;
+        },
+        function() {
+            component.items = [];
+            component.isLoading = false;
+            component.isSuccess = false;
+            component.isFailure = true;
+        }
+    )
 };
 
 var component = {
-    progress: 0,
+    items: [],
+    isLoading: true,
+    isSuccess: false,
+    isFailure: false,
     oninit: oninit,
     view: function() {
-        return m(
-            'div',
-            {
-                class: 'progress',
-            },
-            m(
-                'div', {
-                    class: 'active progress-bar progress-bar-info progress-bar-striped',
-                    style: 'width: ' + this.progress + '%',
-                },
-                '   ' + this.progress + '%   '
-            )
-        );
+        if (component.isLoading) {
+            return m('p', {class: 'text-center'}, m('i', {class: 'fa fa-2x fa-cog fa-spin'}));
+        }
+        if (component.isSuccess) {
+            return m(
+                'table',
+                {class: 'table table-bordered table-hover table-striped'},
+                m('tbody', {}, component.items.map(function(item) {
+                    return [
+                        m(
+                            'tr',
+                            {},
+                            [
+                                m('td', {class: 'text-narrow'}, item.category),
+                                m('td', {class: 'text-narrow text-right'}, item.seeds),
+                                m('td', {}, m('a', {href: item.url}, item.title)),
+                                m('td', {class: 'text-narrow text-center'}, m('a', {href: item.magnet}, 'Magnet')),
+                            ]
+                        ),
+                        m(
+                            'tr',
+                            {},
+                            m('td', {colspan: 4}, item.urls.map(function(url) {
+                                return m('a', {class: 'block', href: url}, url);
+                            }))
+                        ),
+                    ];
+                })
+            ));
+        }
+        if (component.isFailure) {
+            return m('div', {class: 'alert alert-danger'}, 'An unknown error has occurred. Please try again.');
+        }
     },
 };
 
